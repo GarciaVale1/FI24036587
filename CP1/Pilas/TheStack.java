5f6d6d6443acbd6c71b31c8e2cd9a183108f262f
GarciaVale1 <vgarcia70215@ufide.ac.cr>
2025-10-26 17:43:54 -0600

    se corrigio el push,pop,peek

--- CP1/Pilas/TheStack.java
+++ CP1/Pilas/TheStack.java
@@ -15,13 +15,13 @@ public class TheStack<Type> implements TheStackInterface<Type> {
     public Boolean push(Type item) {
         var available = _stack.size() < _capacity;
         if (available) {
-            push(item);
+            _stack.push(item);
         }
         return available;
     }
 
     public Type pop() {
-        return _stack.pop();
+        return _stack.isEmpty()? null : _stack.pop();
     }
 
     public Type peek() {
